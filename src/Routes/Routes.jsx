b1b8b2773b098aeb289107665f799ac63a3f7441
RakibHossen678 <rh491464@gmail.com>
2024-05-06 16:53:56 +0600

    save data to database

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,15 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home";
 import Login from "../Pages/Authentication/Login";
 import Register from "../Pages/Authentication/Register";
+import JobDetails from "../Pages/JobDetails";
+import AddJob from "../Pages/AddJob";
+import ErrorPage from "../Components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
@@ -21,6 +25,15 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register></Register>,
       },
+      {
+        path:'/job/:id',
+        element:<JobDetails></JobDetails>,
+        loader:({params})=>fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`)
+      },
+      {
+        path:'/addJob',
+        element:<AddJob></AddJob>
+      }
     ],
   },
 ]);
